431f3b564cf5086bd6d6860456979ce75f76e212
jstart <11465401+jstart@user.noreply.gitee.com>
2022-10-08 11:45:37 +0800

    关于第一阶段的所有内容

--- src/apiDay04/SortListDemo2.java
+++ src/apiDay04/SortListDemo2.java
@@ -34,7 +34,7 @@ public class SortListDemo2 {
 //        Comparator<String> c = new Comparator<String>() {
 //            @Override
 //            public int compare(String o1, String o2) {
-////                return o1.length()-o2.length();//升序
+//                return o1.length()-o2.length();//升序
 //                return o2.length()-o1.length();//降序
 //            }
 //        };
