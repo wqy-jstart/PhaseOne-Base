fa6c83753c0781ce99cffa54bf948058d913a6d0
jstart <11465401+jstart@user.noreply.gitee.com>
2022-11-05 11:30:01 +0800

    关于第一阶段的所有内容

--- src/apiDay03/CollectionToArrayDemo.java
+++ src/apiDay03/CollectionToArrayDemo.java
@@ -18,7 +18,7 @@ public class CollectionToArrayDemo {
         c.add("five");
         System.out.println(c);
         //若参数数组元素个数==集合元素个数，那就正常转换
-        //若参数数组元素个数>集合元素个数，则正常转换，同时末尾不默认值null
+        //若参数数组元素个数>集合元素个数，则正常转换，同时末尾补默认值null
         //若参数数组元素个数<集合元素个数，则会按照集合大小转给数组
         String[] arr = new String[c.size()];
         //toArray()方法中需要传一个数组以供转换
