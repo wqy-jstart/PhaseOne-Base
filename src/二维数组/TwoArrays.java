4c039f749d797da45ba91b1ad95c17915dbc9423
Devotion <2168149199@qq.com>
2023-01-06 17:36:04 +0800

    。。。

--- src/二维数组/TwoArrays.java
+++ src/二维数组/TwoArrays.java
@@ -2,6 +2,7 @@ package 二维数组;
 
 public class TwoArrays {
     public static void main(String[] args) {
+        System.out.println("=================================================");
         //1）定义一个二维数组：---------静态初始化
         int [][] arrays = new int[2][2];//创建了一个二维数组arrays，里面包含两个一维数组,每个一维数组中各有两个int类型的元素，默认值为0
         int [][] arrays1 = {{1,2},{3,4}};//和arrays数组相同，只是元素确定了
@@ -11,7 +12,9 @@ public class TwoArrays {
         System.out.println(arrays[1][1]);//系统输出arrays中第2个一维数组中的第2个int类型元素,因未修改元素,所以输出默认值0
         System.out.println(arrays1[1][1]);//系统输出arrays1中第2个一维数组中的第2个int类型元素，因已被赋值，故为4
         System.out.println(arrays2[1][0]);//系统输出arrays2中第2个一维数组中的第1个int类型元素，因已被赋值，故为7
+        System.out.println("=================================================");
 
+        System.out.println("=================================================");
         //2）给二维数组中的各个元素赋值：----------动态初始化
         //给二维数组中的第1个一维数组中整型元素赋初始值
         arrays[0] = new int[4];//创建二维数组arrays中的第一个一维数组中包含4个元素
@@ -28,7 +31,19 @@ public class TwoArrays {
         arrays[1][1]=22;//给二维数组arrays中的第2个一维数组中第2个整型元素赋值为22
         arrays[1][2]=33;//给二维数组arrays中的第2个一维数组中第3个整型元素赋值为33
         System.out.println(arrays[1][2]);//系统输出二维数组arrays中第2个一维数组中的第3个整型元素,33
+        System.out.println("=================================================");
 
+        System.out.println("=================================================");
+        // 遍历array二维数组
+        for (int i = 0; i < arrays.length; i++) {
+            for (int j = 0; j < arrays[i].length; j++) {
+                System.out.print(arrays[i][j]+"\t");
+            }
+            System.out.println("");
+        }
+        System.out.println("=================================================");
+
+        System.out.println("=================================================");
         //3）遍历并排列二维数组中的元素：----------利用嵌套for循环
         int[][] a=new int[3][3];//创建一个二维数组a，包含3个一位数组，每一个一维数组中含有3个整型元素，默认值为0
         for (int i = 0; i < a.length; i++) {//嵌套遍历二维数组中的每一个元素
@@ -38,6 +53,7 @@ public class TwoArrays {
             }
             System.out.println(" ");//每一个一维数组遍历结束都会换行
         }
+        System.out.println("=================================================");
         int[][] b = new int[3][3];
         int i = 0;
         int j = 0;
